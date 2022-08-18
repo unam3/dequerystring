0eea80dc273e63e1fa32a8f48fc61f3b13dc53f9
Flowers Safety <40meta@gmail.com>
2022-08-18 12:44:20 +0600

    GHC.Classess.IP(IP) and ip @shit still not working

--- src/DeQueryString.hs
+++ src/DeQueryString.hs
@@ -1,8 +1,31 @@
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DeriveAnyClass #-}
-{-# LANGUAGE ExplicitForAll #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeApplications #-}
 
 module DeQueryString where
 
-g :: forall a b c. (b -> b)
-g = id
+import GHC.Classes
+
+
+-- https://downloads.haskell.org/~ghc/latest/docs/users_guide/exts/scoped_type_variables.html#extension-ScopedTypeVariables
+
+-- a will be the same in f body as in type annotation
+f :: forall a. [a] -> [a]
+f xs = ys ++ ys
+     where
+       ys :: [a]
+       ys = reverse xs
+
+--An equivalent form for that example, avoiding explicit forall uses Pattern type signatures:
+
+f1 :: [a] -> [a]
+f1 (xs :: [aa]) = xs ++ ys
+  where
+    ys :: [aa]
+    ys = reverse xs
+
+instance IP "you" String where
+  ip = "gd"
